52052136f48596199c646d9a77cc05c25d40b562
masamitu-ops <ctpspj6ss2@gmail.com>
2026-01-25 07:08:29 +0000

    サイトタイトルの変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '〇〇の技術ブログ';
+export const SITE_TITLE = 'Masamitsu's Portfolio';
 export const SITE_DESCRIPTION = 'ここへようこそ！';
 
 
